a420cdee7f1269d267adbc40188f14113fcb94b4
Keks <keks@htmlacademy.ru>
2023-12-10 20:50:47 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,21 +1,10 @@
-function checkPalindrome(stroke) {
-  let noSpacedStroke = '';
+function checkPalindrome(string) {
+  let formattedString = string.toLowerCase();
+  const noSpacedString = formattedString.replaceAll(' ', '');
 
-  stroke = stroke.toLowerCase();
+  formattedString = noSpacedString.split('').reverse().join('');
 
-  for(let i = 0; i < stroke.length; i++) {
-    if(stroke[i] !== ' ') {
-      noSpacedStroke += stroke[i];
-    }
-  }
-
-  stroke = noSpacedStroke.split('').reverse().join('');
-
-  if(stroke === noSpacedStroke) {
-    return true;
-  }
-
-  return false;
+  return formattedString === noSpacedString;
 }
 
 checkPalindrome('топот'); // true
@@ -23,28 +12,26 @@ checkPalindrome('ДовОд'); // true
 checkPalindrome('Кекс'); // false
 checkPalindrome('       Лё ша на полке клОПа    нашёл '); // true
 
-function getNumbers(stroke) {
-  let numberedStroke = '';
-  let number;
-  stroke = String(stroke);
+function getNumbers(string) {
+  let numberedString = '';
+  string = String(string);
 
-  for(let i = 0; i < stroke.length; i++) {
-    if(stroke[i] !== ' ') {
-      if(!isNaN(Number(stroke[i]))) {
-        numberedStroke += stroke[i];
-      }
+  for (let i = 0; i < string.length; i++) {
+    if (!isNaN(Number(string[i]))) {
+      numberedString += string[i];
     }
   }
 
-  if(numberedStroke !== '') {
-    number = Number(numberedStroke);
-    return number;
+  numberedString = numberedString.replaceAll(' ', '');
+
+  if (numberedString !== '') {
+    return Number(numberedString);
   }
 
   return NaN;
 }
 
-getNumbers('2023 год'); // 2023
+getNumbers('      2    02 3 год'); // 2023
 getNumbers('ECMAScript 2022'); // 2022
 getNumbers('агент 007'); // 7
 getNumbers(2023); // 2023
@@ -53,30 +40,30 @@ getNumbers(1.5); // 15
 getNumbers('1 кефир, 0.5 батона'); // 105
 getNumbers('а я томат'); // NaN
 
-function addSymbols(stroke, minLength, addingStroke) {
-  if(stroke.length <= minLength) {
-    let newStroke;
-    const addingSymbolsNumber = minLength - stroke.length;
-    const intPartOfStroke = Math.floor(addingSymbolsNumber / addingStroke.length);
-    const restOfStroke = addingSymbolsNumber % addingStroke.length;
-    let totalAddingStroke = '';
+function addSymbols(string, minLength, addingString) {
+  if (string.length <= minLength) {
+    let newString;
+    const addingSymbolsNumber = minLength - string.length;
+    const intPartOfString = Math.floor(addingSymbolsNumber / addingString.length);
+    const restOfString = addingSymbolsNumber % addingString.length;
+    let totalAddingString = '';
 
-    for(let i = 0; i < intPartOfStroke; i++) {
-      totalAddingStroke += addingStroke;
+    for (let i = 0; i < intPartOfString; i++) {
+      totalAddingString += addingString;
     }
 
-    newStroke = totalAddingStroke + stroke;
-    totalAddingStroke = '';
+    newString = totalAddingString + string;
+    totalAddingString = '';
 
-    for(let i = 0; i < restOfStroke; i++) {
-      totalAddingStroke += addingStroke[i];
+    for (let i = 0; i < restOfString; i++) {
+      totalAddingString += addingString[i];
     }
 
-    newStroke = totalAddingStroke + newStroke;
-    return newStroke;
+    newString = totalAddingString + newString;
+    return newString;
   }
 
-  return stroke;
+  return string;
 }
 
 addSymbols('1', 2, '0'); // '01'
@@ -85,14 +72,8 @@ addSymbols('q', 4, 'werty'); // 'werq'
 addSymbols('q', 4, 'we'); // 'wweq'
 addSymbols('qwerty', 4, '0'); // 'qwerty'
 
-function checkStrokeLenght(stroke, maxLenght) {
-  if(stroke.length <= maxLenght) {
-    return true;
-  }
+const checkStringLength = (string, maxLength) => string.length <= maxLength;
 
-  return false;
-}
-
-checkStrokeLenght('проверяемая строка', 20); // true
-checkStrokeLenght('проверяемая строка', 18); // true
-checkStrokeLenght('проверяемая строка', 10); // false
+checkStringLength('проверяемая строка', 20); // true
+checkStringLength('проверяемая строка', 18); // true
+checkStringLength('проверяемая строка', 10); // false
